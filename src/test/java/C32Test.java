d7952f34c032376281989f4330079375379df86a
Xavier <x.sotomayor@alumnos.upm.es>
2018-10-07 14:54:53 +0200

    Add C32Test Class with test for mA() - issue #3

---
+++ src/test/java/C32Test.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C32Test {
+
+    private C32 c32;
+
+    @BeforeEach
+    void before(){
+        this.c32 = new C32();
+    }
+
+    @Test
+    void testMA(){
+        assertEquals("mA", this.c32.mA());
+    }
+}
