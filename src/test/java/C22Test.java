91e559b62b588b6268e972a82cd02d2bfc1a1fb3
Xavier <x.sotomayor@alumnos.upm.es>
2018-10-07 13:01:37 +0200

    Add C22Test Class with test for mA() - issue #2

---
+++ src/test/java/C22Test.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C22Test {
+    private C22 c22;
+
+    @BeforeEach
+    void before(){
+        this.c22 = new C22();
+    }
+
+    @Test
+    void testMA(){
+        assertEquals("mA", this.c22.mA());
+    }
+}
